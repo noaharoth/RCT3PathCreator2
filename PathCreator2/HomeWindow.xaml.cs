b3f74cb6dc91ccc6b7e7652283b7a6868dc98535
Noah Alexander Roth <noaharoth@gmail.com>
2015-11-12 13:36:34 -0700

    Removed temporary test button

--- PathCreator2/HomeWindow.xaml.cs
+++ PathCreator2/HomeWindow.xaml.cs
@@ -15,10 +15,6 @@
 */
 
 using System.Windows;
-using R3ALInterop;
-using System.Text.RegularExpressions;
-using System.IO;
-using System.Diagnostics;
 
 namespace PathCreator
 {
@@ -31,22 +27,8 @@ namespace PathCreator
         public HomeWindow()
         {
 
-            RCT3AssetLibrary.Initialize(null);
-
             InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            MPath path = new MPath();
-
-            path.IsExtended = true;
-
-            OvlModelSearcher searcher = new OvlModelSearcher(null, path);
-
-            var result = searcher.Search(@"C:\Users\noaha\Documents\RCT3 Custom Content\Apps\RCT3 Path Creator\My Paths\test");
-
-            result.ShowResultAsMessageBox();
-        }
     }
 }
